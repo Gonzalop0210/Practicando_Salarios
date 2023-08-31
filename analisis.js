d7cdae5db0d7a25577a88c81bffa0485a676aa44
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-08-31 16:56:16 -0500

    Otra vez

--- analisis.js
+++ analisis.js
@@ -90,6 +90,7 @@ function ordenarPorEmpresaPlatzi(empresa) {
     for (let element of salarios) {
         let arrayTrabajos = element.trabajos;
         let empresaEncontrada = false;
+        let objetoEmpresas = {};
         for (let propiedad of arrayTrabajos) {
             if (propiedad.empresa == empresa && empresaEncontrada == false) {
                 console.log(propiedad.empresa);
