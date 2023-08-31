6e4e051a868cb0e12279bfedbcf07e20fe98dd16
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-08-30 19:48:06 -0500

    Proyección salarial terminado

--- analisis.js
+++ analisis.js
@@ -1,7 +1,7 @@
-/* console.log(salarios[0].name);
- */
-/* let personaBuscada = "Julia";
- */
+/* console.log(salarios[0]); */
+
+/* let personaBuscada = "Julia"; */
+
 function encontrarMedianaSalarial(personaBuscada) {
     let seEncontro = false;
     for (let i = 0; i < salarios.length; i++) {
@@ -15,13 +15,13 @@ function encontrarMedianaSalarial(personaBuscada) {
                     let md = (arrayDentro[item - 1].salario + arrayDentro[item2 - 1].salario) / 2;
                     seEncontro = true;
                     /* console.log("La mediana es: " + md); */
-                    return "La mediana es: " + md;
+                    return md;
                 } else {
                     let item = (arrayDentro.length - 1) / 2;
                     let md = arrayDentro[item].salario;
                     seEncontro = true;
                     /* console.log("La mediana es: " + md); */
-                    return "La mediana es: " + md;
+                    return md;
                 }
             }
             break;
@@ -33,9 +33,37 @@ function encontrarMedianaSalarial(personaBuscada) {
     }
 }
 
-console.log(encontrarMedianaSalarial("Julia"));
+/* console.log(encontrarMedianaSalarial("Juanita")); */
 
 /* 
 Proyeccion = Salaria Actual * (Tasa Crecimiento) ** años proyectados
 Tasa Crecimiento = (sueldo inicial / sueldo final) ** (1 / diferencia de años trabajados) - 1
 */
+
+function proyeccionSalarial(personaBuscada) {
+    for (let element of salarios) {
+        let arrayTrabajos = element.trabajos;
+        if (element.name == personaBuscada) {
+            arrayTrabajos.sort((a,b) => a.year - b.year);
+            let arrayporcentajeCrecimientoAnual = []
+            for (let i = 1; i < arrayTrabajos.length; i++) {
+                let salarioPasado = arrayTrabajos[i - 1].salario;
+                let salarioNuevo = arrayTrabajos[i].salario;
+                let porcentajeCrecimientoAnual = ((salarioNuevo - salarioPasado) / salarioPasado) * 100;
+                arrayporcentajeCrecimientoAnual.push(porcentajeCrecimientoAnual);
+            }
+            let sumaTasasCrecimiento = arrayporcentajeCrecimientoAnual.reduce((acum, num) => acum + num);
+            let promedioTasasCrecimiento = sumaTasasCrecimiento / arrayporcentajeCrecimientoAnual.length;
+            let ultimoSalario = 0;
+            for (let prop of arrayTrabajos) {
+                ultimoSalario = prop.salario;
+            }
+            let proyeccionSalarial =ultimoSalario + (ultimoSalario * (promedioTasasCrecimiento / 100));
+            return "La proyección salarial para el próximo año de " + element.name + " es: " + Math.round(proyeccionSalarial);
+        }
+    }
+}
+
+let persona = "Rigoberto";
+console.log(proyeccionSalarial(persona));
+console.log(encontrarMedianaSalarial(persona));
